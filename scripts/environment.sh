11857d7c73abda7dbaf3dcd5a80ee1557518fc00
Lee Katz - Aspen <gzu2@cdc.gov>
2020-08-21 14:50:14 -0400

    updated environment script

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# this directory doesn't evaluate correctly with dirname $0
+# when being called with 'source ___.sh' and so here is a one-liner
+# https://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 module purge
 module load perl/5.16.1-MT
 
-export PATH=$PATH:$(dirname $0)
+export PATH=$PATH:$DIR
+
